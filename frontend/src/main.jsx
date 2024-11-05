671409337441c71b86d5ab0804db2da8b7cff2cb
Rohit Bhardwaj <154592665+rohit-bhardwajj@users.noreply.github.com>
2024-11-05 18:56:28 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context/authContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <AuthProvider>
         <App />
+        </AuthProvider>
+        <Toaster />
     </React.StrictMode>
 );
