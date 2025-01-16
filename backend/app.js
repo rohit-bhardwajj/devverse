b312046a588288045851d0816e14a360a1840ffc
Rohit Bhardwaj <154592665+rohit-bhardwajj@users.noreply.github.com>
2025-01-16 09:07:25 +0530

    add user routes

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 import express from 'express'
 import cors from 'cors'
-import authRoutes from './routes/authRoutes.js'
 import cookieParser from "cookie-parser";
 
-// import {all} from './routes/blogRoutes'
 
 const app = express()
 app.use(cors());
@@ -13,10 +11,13 @@ app.use(express.json({ limit: "16kb" })); // Parse JSON requests
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.urlencoded({ limit: "16kb" }));
 
-//pls change v1 everywhere i just updated it
-app.use('/api/v1/users', authRoutes); // Mount the auth routes
+//routes import
+import userRouter from './routes/user.routes.js'
+import blogRouter from './routes/blog.routes.js'
+
+app.use('/api/v1/users', userRouter); // Mount the auth routes
 // app.use('/api/v1/snippets', snippetRoutes); // Mount the Snippet route
-// app.use('/api/v1/blogs', blogRoutes); // Mount the Blog route
+app.use('/api/v1/blogs', blogRouter); // Mount the Blog route
 
 
 export {app}
